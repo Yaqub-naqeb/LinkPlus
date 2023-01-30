fdfe2e8a53012b73db20c16999b4af49eeeee8ac
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-01-31 00:16:47 +0300

    structure of navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,10 +3,42 @@ import Link from 'next/link'
 const Navbar = () => {
 
   return (
-    <div className='flex text-red-600'>
-      <div>kdfal</div>
-      
-      <div>kdslafj</div>
+    <div className='flex justify-around '>
+
+
+
+{/* nav */}
+<nav className='flex gap-[10rem]'>
+  {/* logo */}
+<div>logo</div>
+{/* links */}
+  <ul className='flex gap-5'>
+    <li>
+      <Link href={'/'}>home</Link>
+    </li>
+    <li>
+      <Link href={'#'}>projects</Link>
+    </li>
+    <li>
+      <Link href={'#'}>desiners</Link>
+    </li>
+    <li>
+      <Link href={'#'}>login</Link>
+    </li>
+  </ul>
+</nav>
+
+{/* icons */}
+<div className='flex gap-5'>
+
+  <div>search</div>
+  <div>sitteng</div>
+  <div>chatt</div>
+  <div>profile</div>
+
+</div>
+
+  
     </div>
   )
 }
