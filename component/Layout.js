9cc7ec1bc3bc41ab830ca194b40700cbbb3db5b2
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-01-30 23:06:08 +0300

    add

---
+++ component/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Modal from '../redux/reducers/modal'
+
+const Layout = ({children}) => {
+  return (
+    <div>
+     
+
+      <div className=' '>
+      
+      {
+children
+      }
+    </div>
+    </div>
+  )
+}
+
+export default Layout
---
+++ redux/reducers/todoes.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todoesSlice=createSlice({
+
+    name:'todo',
+    // this is initial value
+    initialState:[
+    'barry'
+    ],
+    // to update the initial value
+    reducers:{
+        set_todoes:(state,action)=>{
+           const newOb={
+            id:Date.now(),
+            task:action.payload.task,
+            completed:false
+           }
+           state.push(newOb)},
+// second
+           set_toggle:(state,action)=>{
+            // const toggle=!state.payload.completed
+            const index=state.findIndex(todo=> todo.id===action.payload.id)
+            state[index].completed=action.payload.completed
+
+
+        },
+// third 
+            delete_todoes:(state,action)=>{
+
+                const index=state.findIndex(todo=> todo.id===action.payload.id)
+
+                state.splice(index, 1);
+            },
+//fourth 
+           
+    }
+
+
+
+})
+export const {set_todoes,set_toggle,delete_todoes}=todoesSlice.actions
+export default todoesSlice.reducer
