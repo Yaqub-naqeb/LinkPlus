868d0c2b9fff6fa25b9e9d9d88a1200f097c4f32
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-02-02 15:11:08 +0300

    the card inside profile page finish

--- components/profile/ProfileCard.js
+++ components/profile/ProfileCard.js
@@ -1,9 +1,33 @@
 import React from 'react'
-
+import Image from 'next/image'
+import ProfileImage from '../assets/imgs/profileImg/pexels-spencer-selover-775358.jpg'
 const ProfileCard = () => {
   return (
-    <div>
+    <div className=' w-[289px] h-[628px] rounded-[45px] bg-slate-300 flex gap-5 flex-col items-center justify-center'>
+      <Image src={ProfileImage} className={`w-[209px] h-[303px] rounded-[32px]`} width={300} height={300}/>
+<div className='flex flex-col gap-3 items-center justify-center align-middle'>
+  
+<p className='font-bold'>John derg</p>
+
+<p className='text-[0.8rem]'>Ui/Ux designer</p>
+</div>
+
+    <div className='grid grid-cols-2 gap-x-12 gap-y-3 place-items-center'>
+   <p>Following</p>
+   <p>Follower</p>
+   <p>120</p>
+   <p>135</p>
+    </div>
+
+{/* follow */}
+
       
+      <div className='flex flex-col justify-center items-center gap-5 align-middle'>
+      <button className='bg-[#51557E] rounded-[15px] w-[204px] h-[50px] text-[#E7F6F2
+]'>Follow</button>
+      <button className=' bg-[#757BB8] w-[144px] h-[50px] rounded-[15px]'>Give a task</button>
+      </div>
+
     </div>
   )
 }
--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,10 +1,10 @@
+import ProfileCard from '@/components/profile/ProfileCard'
 import React from 'react'
 
 const index = () => {
   return (
-    <div className='w-full  h-[100vh] '>
-      <h1 className='text-black text-[1rem] '>index
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, temporibus consequuntur vero velit fugiat in, minus nulla totam architecto ipsa voluptatem delectus placeat quas quibusdam soluta dolor? Suscipit, ad praesentium.</h1> 
+    <div className='w-full  h-[100vh]  '>
+   <ProfileCard/>
     </div>
   )
 }
