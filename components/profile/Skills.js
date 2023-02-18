6aef1ec9dd53c99dc77099eba9ac619bb463031d
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-02-18 20:58:17 +0300

    adding array of object

--- components/profile/Skills.js
+++ components/profile/Skills.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import Img1 from '../assets/imgs/logoSkills/1667px-Figma-logo.svg.png'
 import Img2 from '../assets/imgs/logoSkills/adobe-xd-logo-64364E3A24-seeklogo.com.png'
@@ -9,10 +9,54 @@ import Img6 from '../assets/imgs/logoSkills/png-clipart-adobe-indesign-adobe-cre
 import { edit } from '../assets/svg/edit/edit'
 import { useDispatch, useSelector } from 'react-redux'
 import { setSkillsEdit } from '@/redux/reducers/isOpen'
+// import { SkillsIcon } from '../skillsIcon/SkillsIcon'
+import ImageComponent from '../img/ImageComponent'
+
+
+import react from '../assets/skillsIcon/React-icon.svg.png'
+import js from '../assets/skillsIcon/Unofficial_JavaScript_logo_2.svg.png'
+import html from '../assets/skillsIcon/HTML5_logo_and_wordmark.svg.png'
+import css from '../assets/skillsIcon/png-transparent-cascading-style-sheets-logo-css3-html-web-development-world-wide-web-blue-angle-web-design.png'
+
+export const SkillsIcon=[
+  
+  {
+    name:'Html'
+    ,icon:html
+  },{
+    name:'React'
+    ,icon:react
+},{
+    name:'JavaScript'
+    ,icon:js
+},
+{
+    name:'figma'
+    ,icon:Img1
+},
+]
+
+
+
+
 const Skills = () => {
 
+
+
+
+
+
+
+
+
+
+
+
+  
+  const [icons,setIcons]=useState(SkillsIcon);
+  console.log(icons[0]);
+
   const PopUp = useSelector((state) => state.open);
-  console.log(PopUp.skillsEdit);
   const dispatch=useDispatch();
 
 
@@ -23,12 +67,19 @@ const Skills = () => {
       <p className='absolute right-3 cursor-pointer top-8' onClick={()=>dispatch(setSkillsEdit(!PopUp.skillsEdit))}>{edit}</p>
       {/* skills */}
       <div className='grid grid-cols-3 gap-x-2 b gap-y-2'>
-<Image className='h-[40px] w-[30px]'  src={Img1} width={200} height={200}/>
-<Image className='h-[34px] w-[30px]'  src={Img2} width={200} height={200}/>
+
+
+
+
+{
+  SkillsIcon.map((icon,index)=>(<Image key={index} className='h-[40px] w-[40px]' src={icon.icon}   width={800} height={800}/>))
+}
+
+{/* <Image className='h-[34px] w-[30px]'  src={Img2} width={200} height={200}/>
 <Image className='h-[34px] w-[30px]' src={Img3} width={200} height={200}/>
 <Image className='h-[34px] w-[30px]' src={Img4} width={200} height={200}/>
 <Image className='h-[34px] w-[30px]' src={Img5} width={200} height={200}/>
-<Image className='h-[34px] w-[30px]' src={Img6} width={200} height={200}/>
+<Image className='h-[34px] w-[30px]' src={Img6} width={200} height={200}/> */}
 
       </div>
     </div>
---
+++ components/skillsIcon/SkillsIcon.js
@@ -0,0 +1,8 @@
+import {react} from '../assets/skillsIcon/React-icon.svg.png'
+import {js}from '../assets/skillsIcon/Unofficial_JavaScript_logo_2.svg.png'
+export const SkillsIcon=[{
+    name:'React'
+    ,icon:`/assets/skillsIcon/React-icon.svg.png`
+},{
+    name:'JavaScript',icon:'/assets/skillsIcon/Unofficial_JavaScript_logo_2.svg.png'
+},]
