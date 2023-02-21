3276bb5845a9627a0cf7ae5a1a3b89d8b29a7a43
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-02-22 00:04:38 +0300

    add

--- components/profile/ProfileCard.js
+++ components/profile/ProfileCard.js
@@ -6,6 +6,7 @@ import { getAuth } from 'firebase/auth'
 import { edit } from '../assets/svg/edit/edit'
 import { setUploadProfilePhoto } from '@/redux/reducers/isOpen'
 import { useDispatch, useSelector } from 'react-redux'
+import Img1 from '../assets/imgs/profileImg/bg.jpg'
 
 const ProfileCard = () => {
   const photoUrl=useSelector((state) => state.profile);
@@ -25,38 +26,51 @@ const profileUrl= data&&data.filter(name=>name.id==user.uid)
     <div className='row-span-2 self-start place-self-center  w-[289px] h-[628px] rounded-[45px] bg-slate-300 flex gap-5 flex-col items-center justify-center relative'>
 
 
-<div className='absolute right-2 top-5 cursor-pointer' onClick={()=>dispatch(setUploadProfilePhoto(!PopUp.uploadProfilePhoto))}>{edit}</div>
-{/* profile Image Card */}
-      <Image src={profileUrl[0]&&profileUrl[0].profilePhoto} className={`w-[209px] h-[303px] object-cover rounded-[32px]`} width={900} height={900}/>
-   
-<div className='flex flex-col gap-3 items-center justify-center align-middle'>
-  
-<p className='font-bold'>{user.displayName?user.displayName:photoUrl.userName}</p>
 
-<p className=' text-[0.8rem]'>{photoUrl.exprince?photoUrl.exprince:'experince'}</p>
-
-
+<div>
+<Image src={Img1} className={`w-full h-full object-cover rounded-[32px]`} width={900} height={900}/>
 </div>
 
-    <div className='grid grid-cols-2 gap-x-12 gap-y-3 place-items-center'>
-   <p>Following</p>
-   <p>Follower</p>
-   <p>120</p>
-   <p>135</p>
-    </div>
 
-{/* follow */}
+<div className='absolute right-2 top-5 cursor-pointer' onClick={()=>dispatch(setUploadProfilePhoto(!PopUp.uploadProfilePhoto))}>{edit}</div>
 
-      
-      <div className='flex flex-col  justify-center items-center gap-5 align-middle'>
-      <button className={`bg-[#51557E] rounded-[15px] w-[204px] h-[50px] text-[#E7F6F2
-] 
+{/* profile Image Card */}
 
- before:content-[''] before:absolute before:bg-[#757BB8]  before:rounded-[15px]  before:items-center before:justify-center before:opacity-0 before:z-40  hover:before:opacity-80 hover:before:top-0 before:w-0 hover:before:w-full hover:before:flex hover:before:transition-all hover:before:duration-50 hover:before:ease-out before:h-full 
- 
- relative `}><span className=''>Follow</span></button>
-      <button className=' bg-[#757BB8] w-[144px] h-[50px] rounded-[15px]'>Give a task</button>
-      </div>
+<div>
+  
+
+<Image src={profileUrl[0]&&profileUrl[0].profilePhoto} className={`w-[209px] h-[303px] object-cover rounded-[32px]`} width={900} height={900}/>
+   
+   <div className='flex flex-col gap-3  items-center justify-center align-middle'>
+     
+   <p className='font-bold'>{user.displayName?user.displayName:photoUrl.userName}</p>
+   
+   <p className=' text-[0.8rem]'>{photoUrl.exprince?photoUrl.exprince:'experince'}</p>
+   
+   
+   </div>
+   
+       <div className='grid grid-cols-2 gap-x-12 gap-y-3 place-items-center'>
+      <p>Following</p>
+      <p>Follower</p>
+      <p>120</p>
+      <p>135</p>
+       </div>
+   
+   {/* follow */}
+   
+         
+         <div className='flex flex-col  justify-center items-center gap-5 align-middle'>
+         <button className={`bg-[#51557E] rounded-[15px] w-[204px] h-[50px] text-[#E7F6F2
+   ] 
+   
+    before:content-[''] before:absolute before:bg-[#757BB8]  before:rounded-[15px]  before:items-center before:justify-center before:opacity-0 before:z-40  hover:before:opacity-80 hover:before:top-0 before:w-0 hover:before:w-full hover:before:flex hover:before:transition-all hover:before:duration-50 hover:before:ease-out before:h-full 
+    
+    relative `}><span className=''>Follow</span></button>
+         <button className=' bg-[#757BB8] w-[144px] h-[50px] rounded-[15px]'>Give a task</button>
+         </div>
+   
+</div>
 
     </div>
   )
