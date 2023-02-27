c9d10bcfd4d14bfbe81b4774d84d0465ae2cfa30
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-02-27 04:52:01 +0300

    project page problem fixed

--- components/useHooks/useFetch.js
+++ components/useHooks/useFetch.js
@@ -1,23 +1,12 @@
 import { db } from "@/firebase/FirebaseApp";
-import { getAuth } from "firebase/auth";
 import { collection, query,onSnapshot, orderBy, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useSelector } from "react-redux";
-
 
 export const useFetch = (collectionName) => {
-  const PopUp = useSelector((state) => state.open);
-  const [isImageLoad,setIsImageLoad]=useState(false);
-
   const [data, setData] = useState([]);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
-  const auth=getAuth();
-  const {user}=useAuthState(auth)
-  const [subCollectionData,setSubCollection]=useState([]);
 
-  const [allDoc,setAllDoc]=useState([]);
 
   
 useEffect(() => {
--- components/useHooks/useFetchProjects.js
+++ components/useHooks/useFetchProjects.js
@@ -1,24 +1,17 @@
 import { db } from "@/firebase/FirebaseApp";
-import { getAuth } from "firebase/auth";
 import { collection, query,onSnapshot, orderBy, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { useSelector } from "react-redux";
 
 
 export const useFetchProjects = (collectionName) => {
-  const PopUp = useSelector((state) => state.open);
-  const [isImageLoad,setIsImageLoad]=useState(false);
 
   const [data, setData] = useState([]);
   const [isPending, setIsPending] = useState(false);
   const [error, setError] = useState(null);
-  const auth=getAuth();
-  const {user}=useAuthState(auth)
   const [subCollectionData,setSubCollection]=useState([]);
 
-  const [allDoc,setAllDoc]=useState([]);
 
+  const [allDoc,setAllDoc]=useState([]);
   
 useEffect(() => {
   
@@ -33,59 +26,26 @@ useEffect(() => {
     }));
     setData(data);
     setIsPending(false);
-// /////////////////////////////////
-
+// first user second
 data.map( async sub=>{
     // if you want to render all user you can inter sub.docId
     const WorkQ=query(collection(db,`Users/${sub.docId}/moredetail`))
     const workDetal= await getDocs(WorkQ)
     const collectionInfo=workDetal.docs.map(doc=>({
-    ...doc.data(),docId:doc.id,
+    ...doc.data(),docId:doc.id,}))
+    // first Collection moredetail
+      setSubCollection((prev)=>[...prev,collectionInfo]); 
+
+    })
+
     
-    })
-    )
-    // if(collectionInfo.length!=0){
-        // console.log(cloneElement);
-    setSubCollection(collectionInfo);
-    // setAllDoc((prevDocs) => [...prevDocs, collectionInfo]);
-
-      // }
-    })
-
-
-
-
-
-
   });
+ 
   return () => unsubscribe();
 }, []);
 
   return { data,subCollectionData, isPending, error };
 };
 
-    
-  
-// const unsubscribe =async()=>{
 
-    //    data.map( async sub=>{
-    //     // if you want to render all user you can inter sub.docId
-    //     const WorkQ=query(collection(db,`Users/${sub.docId}/moredetail`))
-    //     const workDetal= await getDocs(WorkQ)
-    //     const collectionInfo=workDetal.docs.map(doc=>({
-    //     ...doc.data(),docId:doc.id,
-        
-    //     })
-    //     )
-    //     // if(collectionInfo.length!=0){
-    //         console.log(cloneElement);
-    //     setSubCollection(collectionInfo);
-    //     // setAllDoc((prevDocs) => [...prevDocs, collectionInfo]);
-    
-    //       // }
-    //     })
-
-
-
-// }
 
