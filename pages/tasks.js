b109a6f39926b3907780160d5affcb4f72c50c04
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-04-21 22:09:26 +0300

    make dark mode more flexable

--- pages/tasks.js
+++ pages/tasks.js
@@ -1,15 +1,17 @@
 import General from '@/components/main/General';
 import Tasks from '@/components/tasks/Tasks';
+import { useMode } from '@/components/useHooks/useMode';
 import React from 'react'
 import { useSelector } from 'react-redux';
 
 const tasks = () => {
     const Mode = useSelector((state) => state.open);
+    const {mode}=useMode();
 
   return (
     <div className={`min-h-screen `}>
                 <General/>
-<h1 className='text-[1.2rem] font-semibold text-center pb-10'>Tasks</h1>
+<h1 className={`text-[1.2rem] font-semibold text-center pb-10 ${mode?'text-white':''}`}>Tasks</h1>
       <Tasks/>
     </div>
   )
