5bbea9728d21e5e0442b874e8aa9d0b0520f7b06
Yaqub naqeb <yaqub.009448401@gmail.com>
2023-05-05 00:28:14 +0300

    remove some line code

--- components/useHooks/useMode.js
+++ components/useHooks/useMode.js
@@ -11,27 +11,6 @@ export const useMode = () => {
   const [mode, setMode] = useState();
   const auth=getAuth();
   const [user,loading]=useAuthState(auth)
-// useEffect(() => {
-  
-//   setIsPending(true);
-//   const blogsRef = collection(db, collectionName);
-//   const querySnapshot = query(
-//     blogsRef,
-//     // orderBy('date', sortBy === 'Newest' ? 'desc' : 'asc')
-//     orderBy('timeStamp','desc')
-
-//     );
-//   const unsubscribe = onSnapshot(querySnapshot, (snapshot) => {
-//     const data = snapshot.docs.map((doc) => ({
-//       docId: doc.id,
-//       ...doc.data(),
-//     }));
-//     setData(data);
-//     setIsPending(false);
-//   });
-//   console.log(data);
-//   return () => unsubscribe();
-// }, [PopUp.postPopUp]);
 
 useEffect(()=>{
     const rendering=async()=>{  const q = query(collection(db, "Users"), where("id", "==", user&&user.uid));
